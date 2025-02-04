8485960e8b89094ebbbbe5e685f3140ca3819e80
Cheng Ting Fung <240000541@stu.vtc.edu.hk>
2025-02-04 02:32:53 +0000

    Bug fix

--- app.py
+++ app.py
@@ -4,5 +4,21 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "Hello World! demo"
+@app.route("/index")
+def index():
+    user = {"username": "Miguel"}
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Portland!",
+        },
+        {
+            "author": {"username": "Susan"},
+            "body": "The Avengers movie was so cool!"
+        }, # Extendable
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+if name == "__main__":
+    app.run(debug=True)
