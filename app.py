5dd33bab38ade11f5382c0bfd0a355a2448e6b7c
Cheng Ting Fung <240000541@stu.vtc.edu.hk>
2025-02-04 02:39:59 +0000

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -20,5 +20,5 @@ def index():
     return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-if name == "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
